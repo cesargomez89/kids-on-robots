3e9da372ba7c8c75a7e04b68b8488647fe524097
Cesar Gomez <cesargomez89@gmail.com>
2014-06-27 12:18:32 -0500

    droppables

--- app/scripts/controllers/assembler.js
+++ app/scripts/controllers/assembler.js
@@ -19,9 +19,15 @@ angular.module('kidsOnRobotsApp')
     nxt = main.find('#nxt');
     wheels = main.find('#wheels');
 
-    lightSensor.draggable();
-    motor.draggable();
-    nxt.draggable();
-    wheels.draggable();
+    var options = {
+      stack: '.workplace',
+      cursor: 'move',
+      revert: true
+    };
+
+    lightSensor.draggable(options);
+    motor.draggable(options);
+    nxt.draggable(options);
+    wheels.draggable(options);
   };
 });
