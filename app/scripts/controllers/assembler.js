0f303d67384a7d70c9f603fc4a12f1771094f49d
Cesar Gomez <cesargomez89@gmail.com>
2014-06-28 14:11:33 -0500

    dropped elements

--- app/scripts/controllers/assembler.js
+++ app/scripts/controllers/assembler.js
@@ -4,9 +4,13 @@ angular.module('kidsOnRobotsApp')
 .controller('assemblerCtrl', function ($scope, $document) {
   var main = null;
   var lightSensor = null;
-  var motor = null;
   var nxt = null;
-  var wheels = null;
+  var wheel1 = null;
+  var wheel2 = null;
+  var sensorReceiver = null;
+  var nxtReceiver = null;
+  var wheel1Receiver = null;
+  var wheel2Receiver = null;
 
   $scope.init = function(){
     main = angular.element($document[0].querySelector('.main'));
@@ -15,19 +19,41 @@ angular.module('kidsOnRobotsApp')
 
   $scope.activateSensors = function(){
     lightSensor = main.find('#light-sensor');
-    motor = main.find('#motor');
     nxt = main.find('#nxt');
-    wheels = main.find('#wheels');
+    wheel1 = main.find('#wheel-1');
+    wheel2 = main.find('#wheel-2');
 
-    var options = {
-      stack: '.workplace',
+    sensorReceiver = main.find('#sensor-shape');
+    nxtReceiver= main.find('#nxt-shape');
+    wheel1Receiver = main.find('#wheel-1-shape');
+    wheel2Receiver = main.find('#wheel-2-shape');
+
+    var dragOptions = {
       cursor: 'move',
       revert: true
     };
 
-    lightSensor.draggable(options);
-    motor.draggable(options);
-    nxt.draggable(options);
-    wheels.draggable(options);
+    var dropOptions = {
+      drop: $scope.handleDrop
+    };
+
+    lightSensor.draggable(dragOptions);
+    nxt.draggable(dragOptions);
+    wheel1.draggable(dragOptions);
+    wheel2.draggable(dragOptions);
+
+    sensorReceiver.droppable(dropOptions).data('position', 'center');
+    nxtReceiver.droppable(dropOptions).data('position', 'center');
+    wheel1Receiver.droppable(dropOptions).data('position', 'left');
+    wheel2Receiver.droppable(dropOptions).data('position', 'right');
   };
+
+  $scope.handleDrop = function(event, ui){
+    var position = $(this).data('position');
+    ui.draggable.draggable( 'disable' );
+    $(this).droppable( 'disable' );
+    ui.draggable.position( { of: $(this), my: position+' top', at: position+' top' } );
+    ui.draggable.draggable( 'option', 'revert', false );
+  }
+
 });
